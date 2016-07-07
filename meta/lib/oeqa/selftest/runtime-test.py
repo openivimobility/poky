3adbc086584fa1be5ca74c19fea06878841420e1
Mariano Lopez <mariano.lopez@linux.intel.com>
2016-07-07 14:28:10 +0000

    selftest/runtime-test.py: Add test for testexport SDK feature
    This adds test_testexport_sdk() to test the SDK feature
    of testexport in the CI in order to avoid breaking it.
    
    [YOCTO #9765]
    
    (From OE-Core rev: cf6753df532faff5d6d00bb93fd3de5b7212d2f7)
    
    Signed-off-by: Mariano Lopez <mariano.lopez@linux.intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- meta/lib/oeqa/selftest/runtime-test.py
+++ meta/lib/oeqa/selftest/runtime-test.py
@@ -1,5 +1,5 @@
 from oeqa.selftest.base import oeSelfTest
-from oeqa.utils.commands import runCmd, bitbake, get_bb_var, runqemu
+from oeqa.utils.commands import runCmd, bitbake, get_bb_var, runqemu, updateEnv, restoreEnv
 from oeqa.utils.decorators import testcase
 import os
 
@@ -43,6 +43,64 @@ class TestExport(oeSelfTest):
             failure = True if 'FAIL' in result.output else False
             self.assertNotEqual(True, failure, 'ping test failed')
 
+    def test_testexport_sdk(self):
+        """
+        Summary: Check sdk functionality for testexport.
+        Expected: 1. testexport directory must be created.
+                  2. SDK tarball must exists.
+                  3. Uncompressing of tarball must succeed.
+                  4. Check if the SDK directory is added to PATH.
+                  5. Run tar from the SDK directory.
+        Product: oe-core
+        Author: Mariano Lopez <mariano.lopez@intel.com>
+        """
+
+        features = 'INHERIT += "testexport"\n'
+        # These aren't the actual IP addresses but testexport class needs something defined
+        features += 'TEST_SERVER_IP = "192.168.7.1"\n'
+        features += 'TEST_TARGET_IP = "192.168.7.1"\n'
+        features += 'TEST_SUITES = "ping"\n'
+        features += 'TEST_EXPORT_SDK_ENABLED = "1"\n'
+        features += 'TEST_EXPORT_SDK_PACKAGES = "nativesdk-tar"\n'
+        self.write_config(features)
+
+        # Build tesexport for core-image-minimal
+        bitbake('core-image-minimal')
+        bitbake('-c testexport core-image-minimal')
+
+        # Check for SDK
+        testexport_dir = get_bb_var('TEST_EXPORT_DIR', 'core-image-minimal')
+        sdk_dir = get_bb_var('TEST_EXPORT_SDK_DIR', 'core-image-minimal')
+        tarball_name = "%s.sh" % get_bb_var('TEST_EXPORT_SDK_NAME', 'core-image-minimal')
+        tarball_path = os.path.join(testexport_dir, sdk_dir, tarball_name)
+        self.assertEqual(os.path.isfile(tarball_path), True, "Couldn't find SDK tarball: %s" % tarball_path)
+
+        # Uncompress SDK
+        extract_path = os.path.join(testexport_dir, "sysroot")
+        result = runCmd("%s -y -d %s" % (tarball_path, extract_path))
+
+        # Setting up environment
+        current_env = dict(os.environ)
+        for f in os.listdir(extract_path):
+            if f.startswith("environment-setup"):
+                env_file = os.path.join(extract_path, f)
+                updateEnv(env_file)
+
+        # Check if the SDK directory was added to PATH
+        working_path = False
+        for path in os.environ['PATH'].split(":"):
+            if testexport_dir in path:
+                working_path = True
+                sdk_path = path
+                break
+
+        self.assertEqual(working_path, True, "Couldn't find SDK path in PATH")
+
+        # Execute tar from SDK path
+        cmd_tar = os.path.join(sdk_path, "tar")
+        result = runCmd("%s --version" % cmd_tar)
+
+        restoreEnv(current_env)
 
 class TestImage(oeSelfTest):
 
